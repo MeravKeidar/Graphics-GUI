42e188eb86e10e0ac27ba984e7d22f268a9e6295
OrBroder <or.broder@gmail.com>
2024-03-01 12:16:53 +0200

    camera add

---
+++ Camera.h
@@ -0,0 +1,38 @@
+
+#pragma once
+#include <vector>
+#include <string>
+#include "mat.h"
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+class Camera {
+	GLfloat _left = -1;
+	GLfloat _right = 1;
+	GLfloat _bottom = -1;
+	GLfloat _top = 1;
+	GLfloat _zNear = 1;
+	GLfloat _zFar = -1;
+	int projection_type = 0; //ortho by deafult 
+public:
+	mat4 projection;
+	mat4 cTransform;
+	Camera() : cTransform(mat4(1.0)), projection(mat4(1.0)) {}
+	void setTransformation(const mat4& transform);
+	void LookAt(const vec4& eye, const vec4& at, const vec4& up);
+	void Ortho(const float left, const float right,
+		const float bottom, const float top,
+		const float zNear, const float zFar);
+	void Frustum(const float left, const float right,
+		const float bottom, const float top,
+		const float zNear, const float zFar);
+	void Perspective(const float fovy, const float aspect,
+		const float zNear, const float zFar);
+	void Zoom(GLfloat scale);
+	vec4 eye;
+	vec4 at;
+	vec4 up;
+
+
+};
