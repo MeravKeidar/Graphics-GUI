118ac8ef515ac7f24e87eedd4a7d786b30d76419
OrBroder <or.broder@gmail.com>
2024-03-01 12:26:08 +0200

    light add

---
+++ Light.h
@@ -0,0 +1,56 @@
+
+#pragma once
+#include <vector>
+#include <string>
+#include "mat.h"
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+
+enum LIGHT_TYPE {
+	PARALLEL_LIGHT,
+	POINT_LIGHT
+};
+struct Color {
+	GLfloat r;
+	GLfloat g;
+	GLfloat b;
+
+
+	Color operator*(const GLfloat scale) const {
+		Color result{ r * scale, g * scale, b * scale };
+		return result;
+	}
+
+	void operator*=(GLfloat scale) {
+		Color result{ r * scale, g * scale, b * scale };
+		*this = result;
+	}
+
+	Color operator+(const Color other) const {
+		Color result{ std::min(r + other.r,1.0f),std::min(g + other.g,1.0f),std::min(b + other.b,1.0f) };
+		return result;
+	}
+};
+struct MATERIAL {
+	Color color = { 1.0,0,0 };
+	GLfloat transparency = 1.0;
+	GLfloat diffuse_fraction = 0.2;
+	GLfloat specular_fraction = 0.2;
+	GLfloat ambient_fraction = 0.2;
+};
+class Light
+{
+public:
+
+	GLfloat intensity;
+	LIGHT_TYPE light_type;
+	vec4 direction;
+	vec3 modified_direction;
+	vec4 location;
+	vec3 modified_location;
+	Light() : location(0, 1, 0, 1), direction(0, -1, 0, 0), intensity(0.5) { light_type = POINT_LIGHT; };
+
+
+};
